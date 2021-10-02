e5be1de836e74be50ceeac21c59b658e87825091
polearm123 <shaggywaggy29@gmail.com>
2021-10-02 13:52:05 +0300

    created new repository for part3 backend exercises

---
+++ index.js
@@ -0,0 +1,99 @@
+const { json } = require('express')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+// app.use(morgan('tiny'))
+
+morgan.token('body', function (req, res) { return JSON.stringify(req.body)})
+
+app.use(morgan(':method :url :response-time ms :body'))
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+const generateId = () => {
+  const randomID = Math.floor(Math.random()*1000)
+  return randomID
+}
+
+app.get('/info',(request,response) => {
+  response.send(`<h1>Phonebook has info for ${persons.length} people</h1><p>${new Date()}</p>`)
+})
+
+app.get('/',(request,response) => {
+    response.send(`<h1>hello world! </h1>`)
+  })
+
+app.get('/api/persons',(request,response) => {
+  response.json(persons)
+})
+
+app.post('/api/persons', (request,response) => {
+  
+  const body = request.body
+  const foundPerson = persons.find(person => person.name === body.name)
+  if(!body.name || !body.number){
+    return response.status(404).json({error:"content missing from body of request"})
+  }else if(foundPerson){
+      return response.status(404).json({error:"this person exists already"})
+  }
+
+  const person = {
+    name:body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons=persons.concat(person)
+  response.json(person)
+})
+
+app.get('/api/persons/:id',(request,response) => {
+  console.log(request.params.id)
+  console.log(persons)
+  const foundNote = persons.find( note => parseInt(note.id) === parseInt(request.params.id))
+  if(foundNote){
+    response.json(foundNote)
+  }else{
+    response.status(400).send("this note doesn't exist")
+  }
+})
+
+app.delete(`/api/persons/:id`,(request,response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  
+  response.status(204).end()
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
